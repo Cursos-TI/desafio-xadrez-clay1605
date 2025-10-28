41b93299e504004fca7461dd3c4db3ca9bb516fe
clay1605 <clay1605@hotmail.com>
2025-10-28 13:17:35 +0000

    Xadrez nível 1

---
+++ xadreznovato.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+
+    int i = 1;
+    /**/
+while ( i <= 5){
+    printf("Torre moveendo para a direita\n");
+    i++;
+}
+/*
+    do{
+        printf("Bispo movendo em diagonal para cima a direita.\n");
+        i++;
+    } while ( i <=  5);
+
+    for (i = 1; i <= 8; i++)
+    {
+        printf("Rainha se movendo a esquerda.\n");
+    } */
+ return 0;
+
+}
